624cade787312dafd080ad5ca5680622694d22aa
SudharsaneSivamany <sudharsanes694@gmail.com>
2023-11-01 12:21:41 +0530

    updeated output

--- management/outputs.tf
+++ management/outputs.tf
@@ -1,7 +1,14 @@
-output "folder_id" {
+output "folder_ids" {
   value = module.folders.folder_id
 }
 
-output "project_id" {
-  value = [for entry in var.foundation_hierarchy.projects : { "${entry.displayName}" = module.projects[entry.displayName].project_id }]
+output "project_ids" {
+  value = {
+    for entry in var.foundation_hierarchy.projects : entry.displayName => module.projects[entry.displayName].project_id
+  }
+}
+output "lien_name" {
+  value = {
+    for entry in var.foundation_hierarchy.projects : entry.displayName => google_resource_manager_lien.lien[entry.displayName].name if length(entry.lienReason) > 0
+  }
 }
