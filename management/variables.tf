d57a216e8de1c1f1f54f3f0f908ec9ddb5e208f8
Rahul Kumar <rahkumar49@publicisgroupe.net>
2023-12-18 15:31:01 +0530

    Adding budget code under management

--- management/variables.tf
+++ management/variables.tf
@@ -142,3 +142,27 @@ variable "test_flag" {
   type    = bool
   default = false
 }
+
+variable "budget_config" {
+  description = "Configuration for setting GCP budget"
+  type = object({
+    billing_account                  = string
+    projects                         = list(string)
+    create_budget                    = bool
+    display_name                     = string
+    credit_types_treatment           = string
+    services                         = list(string)
+    calendar_period                  = string
+    custom_period_start_date         = string
+    custom_period_end_date           = string
+    alert_spent_percents             = list(number)
+    alert_spend_basis                = string
+    alert_pubsub_topic               = string
+    monitoring_notification_channels = list(string)
+    labels                           = map(string)
+    budget_amount_sandbox            = string
+    budget_amount_default            = string
+    currency_code                    = string
+    project_prefix                   = optional(string)
+  })
+}
