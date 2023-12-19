80a51c0dd4a30d302aadafd52e0d97b7f71d1a6e
Rahul Kumar <rahkumar49@publicisgroupe.net>
2023-12-19 11:15:41 +0530

    teast

--- management/budget.tf
+++ management/budget.tf
@@ -1,6 +1,7 @@
+
 module "budget" {
   source   = "github.com/XBankGCPOrg/gcp-lz-modules//budget/?ref=ba138b9f9ebd8a9048ae39e54ddf0b82c2cdb707"
-  for_each = var.budget_config
+  for_each = { for idx, config in var.budget_config : idx => config }
 
   budget_config = each.value
 
