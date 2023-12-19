37b9810557adddb365cdea1064a9e864bd59fb52
Rahul Kumar <rahkumar49@publicisgroupe.net>
2023-12-19 10:48:40 +0530

    Adding loop

--- management/budget.tf
+++ management/budget.tf
@@ -1,5 +1,7 @@
 module "budget" {
-  source = "github.com/XBankGCPOrg/gcp-lz-modules//budget/?ref=ba138b9f9ebd8a9048ae39e54ddf0b82c2cdb707"
-  budget_config=var.budget_config
-  
+  source   = "github.com/XBankGCPOrg/gcp-lz-modules//budget/?ref=ba138b9f9ebd8a9048ae39e54ddf0b82c2cdb707"
+  for_each = var.budget_config
+
+  budget_config = each.value
+
 }
