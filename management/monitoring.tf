bb08b86bad28e2e091125f8ef2463f8acc61f56d
Rahul Kumar <rahkumar49@publicisgroupe.net>
2023-12-22 09:57:07 +0530

    Updated hash

--- management/monitoring.tf
+++ management/monitoring.tf
@@ -1,6 +1,6 @@
 
 module "monitoring" {
-  source = "github.com/XBankGCPOrg/gcp-lz-modules//monitoring/?ref=a6c3737c54665b45808006f101b9ef525b7cc268"
+  source = "github.com/XBankGCPOrg/gcp-lz-modules//monitoring/?ref=9784267f6e832e1a88561108b5975bb43c005111"
 
   for_each          = { for entry in var.monitoring_config : entry.project_id => entry }
   monitoring_config = each.value
