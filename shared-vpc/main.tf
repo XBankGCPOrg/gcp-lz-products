c44f9ab6918d0d2d236279cfa7264a7966e508e0
Rahul Kumar <rahkumar49@publicisgroupe.net>
2024-01-15 10:29:59 +0530

    Updated code for shared vpc

--- shared-vpc/main.tf
+++ shared-vpc/main.tf
@@ -1,5 +1,5 @@
 module "shared-VPC" {
-  source     = "github.com/XBankGCPOrg/gcp-lz-modules//shared_vpc?ref=769f7203adff0373b4c7d231bb3bdb132ad0fff2"
+  source     = "github.com/XBankGCPOrg/gcp-lz-modules//shared_vpc?ref=6b9922e6d2a377133838fd3dbbafce0c122c85d4"
   for_each   = { for entry in var.shared_vpc : entry.service_project => entry }
   shared_vpc = each.value
 }
