66ea4f52182bda162ed360fd47f2e2ceee3384c5
Rahul Kumar <rahkumar49@publicisgroupe.net>
2024-01-15 10:01:42 +0530

    Updating the shared vpc module

--- shared-vpc/main.tf
+++ shared-vpc/main.tf
@@ -1,5 +1,5 @@
 module "shared-VPC" {
-  source     = "github.com/XBankGCPOrg/gcp-lz-modules//shared_vpc?ref=8174380ed7cf8ee97ac4fae29989045196460a43"
-  for_each      = { for entry in var.shared_vpc : entry.service_project => entry }
+  source     = "github.com/XBankGCPOrg/gcp-lz-modules//shared_vpc?ref=769f7203adff0373b4c7d231bb3bdb132ad0fff2"
+  for_each   = { for entry in var.shared_vpc : entry.service_project => entry }
   shared_vpc = each.value
 }
--- shared-vpc/variables.tf
+++ shared-vpc/variables.tf
@@ -2,5 +2,7 @@ variable "shared_vpc" {
   type = list(object({
     project         = string
     service_project = string
+    service_account = string
+    subnet          = string
   }))
 }
