6b4bb751d0ee8d1632e3b5866d93c99ba1562340
Rahul Kumar <rahkumar49@publicisgroupe.net>
2024-01-16 09:42:27 +0530

    Updated code for budget

--- management/budget.tf
+++ management/budget.tf
@@ -1,6 +1,6 @@
 
 module "budget" {
-  source = "github.com/XBankGCPOrg/gcp-lz-modules//budget/?ref=1afce8e3476c3b6e5522d102ba07b3c18caa7d82"
+  source = "github.com/XBankGCPOrg/gcp-lz-modules//budget/?ref=f679c67a597250f5488659d2d3d37e2301c20112"
 
   for_each      = { for entry in var.budget_config : entry.display_name => entry }
   budget_config = each.value
--- management/variables.tf
+++ management/variables.tf
@@ -146,24 +146,24 @@ variable "test_flag" {
 variable "budget_config" {
   description = "Configuration for setting GCP budget"
   type = list(object({
-    billing_account                  = string
-    projects                         = list(string)
-    create_budget                    = bool
-    display_name                     = string
-    credit_types_treatment           = string
-    services                         = list(string)
-    calendar_period                  = string
-    custom_period_start_date         = string
-    custom_period_end_date           = string
-    alert_spent_percents             = list(number)
-    alert_spend_basis                = string
+    billing_account          = string
+    projects                 = list(string)
+    create_budget            = bool
+    display_name             = string
+    credit_types_treatment   = string
+    services                 = list(string)
+    calendar_period          = string
+    custom_period_start_date = string
+    custom_period_end_date   = string
+    alert_spent_percents     = list(number)
+    alert_spend_basis        = string
     # alert_pubsub_topic               = string
     # monitoring_notification_channels = list(string)
-    labels                           = map(string)
-    budget_amount_sandbox            = string
-    budget_amount_default            = string
-    currency_code                    = string
-    project_prefix                   = optional(string)
+    labels                = map(string)
+    budget_amount_sandbox = string
+    budget_amount_default = string
+    currency_code         = string
+    project_prefix        = optional(string)
   }))
 }
 
@@ -256,8 +256,8 @@ variable "monitoring_config" {
     bandwidth_alert_per_series_aligner      = string
     bandwidth_alert_combiner                = string
 
-   # monitoring_notification_channels = list(string)
-    email_notification_display_name  = string
-    email_notification_address       = string
+    # monitoring_notification_channels = list(string)
+    email_notification_display_name = string
+    email_notification_address      = string
   }))
 }
